194ff887622449315ea4644741cd3d74f129a4b5
Ryan Koesters <rmkoesters@gmail.com>
2013-05-08 13:36:11 -0500

    More work on config parser.

--- config.go
+++ config.go
@@ -5,9 +5,12 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
 func parseConfig(ch chan map[string]string) {
+	var m map[string]string
+
 	f, err := os.Open(*config)
 	if err != nil {
 		log.Fatalf("Error opening config file: %v", err)
@@ -19,11 +22,21 @@ func parseConfig(ch chan map[string]string) {
 	for {
 		line, err := conf.ReadString('\n')
 		if err != nil && err != io.EOF {
-			log.Print(err)
+			log.Fatal(err)
 		}
 		if err != nil && err == io.EOF {
 			break;
 		}
 
+		line = strings.TrimSpace(line)
+		switch line[0] {
+		case "#":
+			// This line is a comment, lets skip it.
+		case "[":
+			// We reached a new header, so lets create a new map.
+			m = make(map[string]string)
+			m["mount"] = line[1:len(line)-1]
+		default:
+			
 	}
 }
