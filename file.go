5bc7f749b61db236e2226678fceeba7813dbf149
Ryan Koesters <rmkoesters@gmail.com>
2013-05-16 13:15:09 -0500

    file.go: Use `http.ServeFile'.

--- file.go
+++ file.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"io"
-	"log"
 	"net/http"
-	"os"
 )
 
 type file string
@@ -14,16 +11,5 @@ func File(config map[string]string) http.Handler {
 }
 
 func (path file) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	f, err := os.Open(string(path))
-	if err != nil {
-		log.Print(err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	defer f.Close()
-
-	_, err = io.Copy(w, f)
-	if err != nil {
-		log.Printf("error: io.Copy: %v", err)
-	}
+	http.ServeFile(w, r, string(path))
 }
