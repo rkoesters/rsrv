3cc307f70be62dd58c642750337c7e6c93464bb8
Ryan Koesters <rmkoesters@gmail.com>
2013-05-16 12:51:44 -0500

    handler.go: Renamed `NewHandler' map to `handlers'.

--- handler.go
+++ handler.go
@@ -7,7 +7,7 @@ import (
 
 // This map contains all of the functions that we can use
 // to create handlers.
-var newHandler = map[string]func(map[string]string) http.Handler{
+var handlers = map[string]func(map[string]string) http.Handler{
 	"dir":  newDirHandler,
 	"file": newFileHandler,
 }
@@ -16,7 +16,7 @@ func getHandler(config map[string]string) http.Handler {
 	t := mustGet(config, "type")
 
 	// Get the function that creates our handler.
-	f, ok := newHandler[t]
+	f, ok := handlers[t]
 	if !ok {
 		log.Fatalf("error: unknown type: %v", t)
 	}
