e18d77213e8d81fac12d438b48707c8a937b2af5
Ryan Koesters <rmkoesters@gmail.com>
2013-05-17 16:37:03 -0500

    servemux.go: Created wrapper for http.ServeMux.

---
+++ servemux.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"net/http"
+)
+
+// ServeMux is a wrapper around http.ServeMux in order to work well
+// with http.StripPrefix.
+type ServeMux struct {
+	mux    *http.ServeMux
+	prefix string
+}
+
+func NewServeMux(prefix string) *ServeMux {
+	m := new(ServeMux)
+
+	m.prefix = prefix
+	m.mux = http.NewServeMux()
+
+	return m
+}
+
+func (m *ServeMux) Prefix(s string) string {
+	return m.prefix + s
+}
+
+func (m *ServeMux) Handle(pattern string, handler http.Handler) {
+	m.mux.Handle(m.Prefix(pattern), handler)
+}
+
+func (m *ServeMux) HandleFunc(pattern string, handler func(http.ResponseWriter, *http.Request)) {
+	m.mux.HandleFunc(m.Prefix(pattern), handler)
+}
+
+func (m *ServeMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	r.URL.Path = m.Prefix(r.URL.Path)
+	m.mux.ServeHTTP(w, r)
+}
