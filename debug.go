444b7053a8484100afe793f7464ec2b9d709ca87
Ryan Koesters <rmkoesters@gmail.com>
2013-05-19 22:12:07 -0500

    debug.go: Started index page for debug.

--- debug.go
+++ debug.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"net/http"
+	"strings"
+	"time"
 )
 
 func Debug(config map[string]string) http.Handler {
@@ -9,7 +11,20 @@ func Debug(config map[string]string) http.Handler {
 
 	mux := NewServeMux(mount)
 
+	mux.HandleFunc("/", debugIndex)
 	mux.Handle("/config", fileHandler(*configFile))
 
 	return mux
+}
+
+var debugIndexPage = `
+<pre>
+<a href="config">Configuration File</a>
+</pre>
+`
+
+func debugIndex(w http.ResponseWriter, r *http.Request) {
+	page := strings.NewReader(debugIndexPage)
+
+	http.ServeContent(w, r, "index.html", time.Now(), page)
 }
